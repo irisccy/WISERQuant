dc70d85d9d51bd7c3b881a6f6fe74b3a175b5222
zhichaoluo <zhichao.luo@gmail.com>
2016-10-15 07:49:24 +0800

    input con

--- getData.R
+++ getData.R
@@ -2,11 +2,12 @@
 getData<-function(stock,from,to){
 library(RJDBC)
   drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver","sqljdbc4.jar")
-  #con COPY INTO HERE
-  
-  # END OF COPY INTO HERE
-   getSql<-paste0("tempdata..get_data ", "'",stock,"'",',',"'",from,"'",',',"'",to,"'")
+  con <- dbConnect(drv, url="jdbc:sqlserver://172.16.8.48", user="stock", password="wisesoe.002",dbname="tempdata")
+  getSql<-paste0("tempdata..get_data ", "'",stock,"'",',',"'",from,"'",',',"'",to,"'")
   ds<-dbGetQuery(con, getSql)
   dbDisconnect(con)
   return(ds)
 }
+
+#sample 
+#getData("000001",201601,201609)
