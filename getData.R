3a733d7ba8b16cfbc2aacac74b60bfa44536451a
zhichaoluo <zhichao.luo@gmail.com>
2016-10-15 07:50:53 +0800

    update ip

--- getData.R
+++ getData.R
@@ -2,7 +2,7 @@
 getData<-function(stock,from,to){
 library(RJDBC)
   drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver","sqljdbc4.jar")
-  con <- dbConnect(drv, url="jdbc:sqlserver://172.16.8.48", user="stock", password="wisesoe.002",dbname="tempdata")
+  con <- dbConnect(drv, url="jdbc:sqlserver://121.192.176.98", user="stock", password="wisesoe.002",dbname="tempdata")
   getSql<-paste0("tempdata..get_data ", "'",stock,"'",',',"'",from,"'",',',"'",to,"'")
   ds<-dbGetQuery(con, getSql)
   dbDisconnect(con)
