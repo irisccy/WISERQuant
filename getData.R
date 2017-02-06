0d77a0350b67a5074d01eb16611daca59ad3e0af
zhichaoluo <zhichao.luo@gmail.com>
2017-02-06 20:59:02 +0800

    update userid and passd

--- getData.R
+++ getData.R
@@ -2,12 +2,13 @@
 getData<-function(stock,from,to){
 library(RJDBC)
   drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver","sqljdbc4.jar")
-  con <- dbConnect(drv, url="jdbc:sqlserver://121.192.176.98", user=" ", password=" ",dbname="tempdata")
+  con <- dbConnect(drv, url="jdbc:sqlserver://121.192.176.98", user="stock", password="wisesoe.002",dbname="tempdata")
   getSql<-paste0("tempdata..get_data ", "'",stock,"'",',',"'",from,"'",',',"'",to,"'")
   ds<-dbGetQuery(con, getSql)
   dbDisconnect(con)
   return(ds)
 }
+
 
 #sample 
 s0102<-getData("399300",201601,201601)
