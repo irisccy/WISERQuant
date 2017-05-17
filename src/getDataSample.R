518b1312038ee59b5c39051beb1e7227c3d5c1d9
zhichaoluo <zhichao.luo@gmail.com>
2017-05-17 08:29:03 +0800

    translate to english

--- src/getDataSample.R
+++ src/getDataSample.R
@@ -1,25 +1,26 @@
 #sample code 
 source("src/getGtaData.r")
 source("src/conGta.r")
-#请联系相关老师获得conGta.r文件中的用户名及密码
+#Please contact your teacher to get the user name and passd in conGta.R
 
-#获取期货分钟交易数据
+#get commodity futures trade minute data
 #t1="MFL1" t2="TRDMIN" 
 ds<-getGtaData(code="A1705",t1="MFL1",t2="TRDMIN",f="05",from="20170101",to="20170201")
 
-#获取股票分钟交易数据
+#get stock trade minute data
 #t1="SEL1" t2="TRDMIN"
 ds<-getGtaData(code="000858.SZ",t1="SEL1",t2="TRDMIN",f="60",from="20170101",to="20170201")
 
-#获取深圳市场股票高频逐笔交易数据
+#get ShenZhen stock transaction L2 data (by each order number)
 #t1="SZL2" t2="Trade"
 ds<-getGtaData(code="000858.SZ",t1="SZL2",t2="Trade",from="20170101",to="20170201")
 
-#获取上海市场股票高频逐笔交易数据
+#get ShangHai stock transaction L2 data (by each order number)
 #t1="SHL2" ,t2="TRANSACTION",t3="SEL2"
 ds<-getGtaData(code="600104.SH",t1="SHL2" ,t2="TRANSACTION",t3="SEL2",from="20170101",to="20170201")
 
-#获取股票市场TAQ交易数据
+#get stock TAQ data
 #t1="SEL1" ,t2="TAQ"
+#data license end in 2016-12-31
 ds<-getGtaData(code="600104.SH",t1="SEL1" ,t2="TAQ",from="20160101",to="20161231")
 
